0d32097b0782efca259bc72f8691883cdaf50071
Markus Auvo <Markus Auvo>
2016-11-14 08:05:28 +0200

    Commit #7: Added application's main test class, MainTest.java

--- app/test/Datastructure_lab_2016/MainTest.java
+++ app/test/Datastructure_lab_2016/MainTest.java
@@ -4,8 +4,11 @@ import Datastructure_lab_2016.algo.*;
 import Datastructure_lab_2016.util.*;
 
 /**
+ * Application's main test class which runs all
+ * the application's tests in one run. This is
+ * where you add and implement unit tests.
  *
- * @author mjauv
+ * @author Markus J. Auvo 2016
  */
 public class MainTest {
     private static final JavaDESTest JDT = new JavaDESTest();
