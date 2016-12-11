e747db41978019803b0f3229bd8c936f7bbcdba5
Markus Auvo <Markus Auvo>
2016-12-11 19:30:01 +0200

    Commit #23: Modified main class Main.java

--- app/src/Datastructure_lab_2016/Main.java
+++ app/src/Datastructure_lab_2016/Main.java
@@ -1,21 +1,17 @@
 package Datastructure_lab_2016;
 
-import Datastructure_lab_2016.algo.*;
+import Datastructure_lab_2016.view.*;
 
 /**
  * Lab assignment for Datastructures and Algorithms
  * Intermediate studies @ University of Helsinki.
- * 
+ * <p>
  * Purpose of the assignment is to compare the performance between
  * Java's own crypto tools and crypto algorithm developed by the
- * student using picture files of different size (0.5MB, 1MB, 2MB).
- * 
+ * student using image files of different sizes.
+ * <p>
  * This algorithm implements Data Encryption Standard (DES).
- *
- * This application does not implement MVC design pattern, since the focus
- * is in the implementation of an encryption algorithm and the text based
- * user interface is meant only to provide means to serve this purpose.
- *
+ * <p>
  * @author Markus J. Auvo 2016
  */
 public class Main
@@ -23,40 +19,9 @@ public class Main
     /**
      * Main method and entry point to application
      * 
-     * @param args
-     * @throws Exception 
+     * @param args 
      */
-    public static void main(String[] args) throws Exception {
-
-        String FILE_NAME = "Picture3";
-
-        int ENCRYPTION = 0;
-        int DECRYPTION = 1;
-
-        String ORIGINAL_FILE = "data/" + FILE_NAME + ".ORIGINAL.jpg";
-        String ENCRYPT_JAVA_FILE  = "data/" + FILE_NAME + ".ENCRYPTED.JavaWay.jpg";
-        String DECRYPT_JAVA_FILE  = "data/" + FILE_NAME + ".DECRYPTED.JavaWay.jpg";
-        String ENCRYPT_MY_FILE  = "data/" + FILE_NAME + ".ENCRYPTED.MyWay.jpg";
-        String DECRYPT_MY_FILE  = "data/" + FILE_NAME + ".DECRYPTED.MyWay.jpg";
-
-        /**
-         * JavaDES
-         */
-
-        //System.out.println();
-        //System.out.println("## JavaDES: " + FILE_NAME + " ##");
-        //JavaDES JavaWay = new JavaDES();
-        //JavaWay.encryptImage(ORIGINAL_FILE, ENCRYPT_JAVA_FILE);
-        //JavaWay.decryptImage(ENCRYPT_JAVA_FILE, DECRYPT_JAVA_FILE);
-
-        /**
-         * MyDES
-         */
-
-        System.out.println();
-        System.out.println("## MyDES: " + FILE_NAME + " ##");
-        MyDES MyWay = new MyDES();
-        MyWay.execute(ENCRYPTION, ORIGINAL_FILE, ENCRYPT_MY_FILE);
-        MyWay.execute(DECRYPTION, ENCRYPT_MY_FILE, DECRYPT_MY_FILE);
+    public static void main(String[] args) {
+        GUI gui = new GUI();
     }
 }
